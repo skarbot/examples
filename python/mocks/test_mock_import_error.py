adadb473f7cd70d50132a69f715d4205e5857d46
Kartik Sundararajan <kartiksundarajan@gmail.com>
2017-03-13 22:54:31 +1300

    Add mock example for import error check

---
+++ python/mocks/test_mock_import_error.py
@@ -0,0 +1,25 @@
+import builtins
+from unittest.mock import patch
+from unittest import TestCase, main
+
+
+def module_check():
+    try:
+        import abc
+    except ImportError:
+        return 1
+
+realimport = builtins.__import__
+def myimport(name, globals, locals, fromlist, level):
+    if name == 'abc':
+        raise ImportError
+    return realimport(name, globals, locals, fromlist, level)
+
+
+class TestModuleImport(TestCase):
+    def test(self):
+        with patch('builtins.__import__', myimport):
+            self.assertEqual(module_check(), 1)
+
+if __name__ == '__main__':
+    main()
